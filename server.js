d70d6bdab3190e3a2d8fa1723ec026a81a2f1b66
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2019-05-18 13:57:01 +0100

    setting up requests and parsing

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+var fs = require("fs");
 var express = require("express");
+var formidable = require("express-formidable");
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send("Yay Node Girls!");
+app.use(formidable());
+app.use(express.static("public"));
+
+// fs.writeFile("location-of-your-file-goes-here", yourData, function(error) {
+//   // do something
+// });
+
+fs.readFile(__dirname + "/data/posts.json", function(error, file) {
+   var parsedFile = JSON.parse(file);
+   console.log("parsedFile->", parsedFile);
+});
+
+// app.get("/chocolate", function(req, res) {
+//   res.send("Mm chocolate :O");
+// });
+app.post("/create-post", function(req, res) {
+//   console.log("req.body", req.body);
+  console.log("req.fields", req.fields);
+//   res.send("POST request to the homepage");
 });
 
 app.listen(3000, function() {
